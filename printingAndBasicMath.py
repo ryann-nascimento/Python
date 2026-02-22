cd56c12a5a14a78b5b3cb605193ff4ef249f0e06
Ryann Nascimento <ryanncarvalho21@gmail.com>
2026-02-22 19:49:29 -0300

    Add a new exercise line

--- printingAndBasicMath.py
+++ printingAndBasicMath.py
@@ -2,3 +2,12 @@ name = "Ryann Nascimento"
 age = 22
 
 print("Hello, my name is", name, "and I am", age, "years old.")
+
+
+# Another Exercise 
+
+server_cost = 1200
+quantity = 3
+total = str(server_cost * quantity)
+
+print("The total cost for the servers is: $" + total)
